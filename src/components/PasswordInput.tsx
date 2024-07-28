045126dccdb5959d0e52cd27eefe157d63bc5c0d
Suyog Shrestha <suyogstha317@gmail.com>
2024-07-28 20:38:37 +0545

    navbar created

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -19,7 +19,7 @@ const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
           type="button"
           onClick={() => setShowPassword(!showPassword)}
           title={showPassword ? "Hide password" : "Show password"}
-          className="absolute right-3 top-1/2 -translate-y-1/2 transform text-muted-foreground"
+          className="absolute right-3 top-1/2 size-4 -translate-y-1/2 transform text-muted-foreground"
         >
           {showPassword ? (
             <EyeOff className="size-4" />
