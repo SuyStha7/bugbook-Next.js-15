7c64298943a735f9a75d36f07e5a1525b85534fe
Suyog Shrestha <suyogstha317@gmail.com>
2024-07-30 18:49:59 +0545

    deleted post

---
+++ src/components/posts/mutation.ts
@@ -0,0 +1,59 @@
+import { useToast } from "@/components/ui/use-toast";
+import {
+  InfiniteData,
+  QueryFilters,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { PostsPage } from "@/lib/types";
+import { usePathname, useRouter } from "next/navigation";
+import { deletePost } from "./action";
+
+export function useDeletePostMutation() {
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const mutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: async (deletedPost) => {
+      const queryFilter: QueryFilters = { queryKey: ["post-feed"] };
+
+      await queryClient.invalidateQueries(queryFilter);
+
+      queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
+        queryFilter,
+        (oldData) => {
+          if (!oldData) return;
+
+          return {
+            pageParams: oldData.pageParams,
+            pages: oldData.pages.map((page) => ({
+              nextCursor: page.nextCursor,
+              posts: page.posts.filter((p) => p.id !== deletedPost.id),
+            })),
+          };
+        },
+      );
+      toast({
+        description: "Post deleted",
+      });
+
+      if (pathname === `/post/${deletedPost.id}`) {
+        router.push(`/users/${deletedPost.user.username}`);
+      }
+    },
+    onError(error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Failed to delete post. Please try again!",
+      });
+    },
+  });
+
+  return mutation;
+}
